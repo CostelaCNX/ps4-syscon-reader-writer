0a0350f7ed2bce7f6b8a482a0ed3af4d3d8c0ab3
CostelaCNX <razrconteudo@gmail.com>
2025-02-24 22:18:24 -0300

    Python 3 Support
    Code update to work using Python 3

--- Reader.py
+++ Reader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import filecmp
 import serial
@@ -27,7 +27,7 @@ def print_banner():
     print(Fore.BLACK + Back.CYAN + "*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*" + Back.BLACK + Style.RESET_ALL + "\n")
 
 
-os.system('cls')
+os.system('cls' if os.name == 'nt' else 'clear')
 print_banner()
 
 syscon_hashes = {
@@ -59,65 +59,64 @@ syscon_hashes = {
     "2.23_3D": "F49E143B692CAF13688ED853874C7390",
     "2.23_4D": "27CA52CADF19BA6A33D2DF321A1FC0B2",
     "2.26D": "62703902E60DFDCC3FDB8749433DBFEC",
-    
 }
+
 def dump(s):
-    #print "=======================================================\n"
-    print "Opening Serial Port: {}".format(port.upper())
+    print("Opening Serial Port: {}".format(port.upper()))
     try:
         ser = serial.Serial(port, baudrate=115200, timeout=5)
     except serial.serialutil.SerialException as e:
         print(Fore.RED + "\nError: Unable to open serial port " + port + Style.RESET_ALL)
-        print ("\nPress Enter to Exit...")
-        raw_input()  
-        sys.exit(1) 
-    print "\nWaiting...\n"
+        print("\nPress Enter to Exit...")
+        input()
+        sys.exit(1)
+    print("\nWaiting...\n")
     time.sleep(2)
-    ser.write(chr(0x00))
+    ser.write(bytes([0x00]))
     size = 0
     start_time = time.time()
     threshold = 360
     while size == 0:
         elapsed_time = time.time() - start_time
         if elapsed_time > threshold:
-            print "{0:60}".format(Fore.RED + "\nChip Timeout! Check Connections!\n" + Style.RESET_ALL),'\r',
+            print("{0:60}".format(Fore.RED + "\nChip Timeout! Check Connections!\n" + Style.RESET_ALL), end='\r')
             sys.stdout.flush()
-            print ("\nPress Enter to Exit...")
-            raw_input()  
+            print("\nPress Enter to Exit...")
+            input()
             sys.exit(1)
-        try:            
+        try:
             resp = ser.read(1)
         except serial.serialutil.SerialException as e:
-            print "{0:60}".format(Fore.RED + "\nSerial Exception: Access Denied (USB Unplugged?)\n" + Style.RESET_ALL),'\r',
+            print("{0:60}".format(Fore.RED + "\nSerial Exception: Access Denied (USB Unplugged?)\n" + Style.RESET_ALL), end='\r')
             sys.stdout.flush()
-            print ("\nPress Enter to Exit...")
-            raw_input()  
+            print("\nPress Enter to Exit...")
+            input()
             sys.exit(1)
         except KeyboardInterrupt:
-            print "{0:60}".format(Fore.RED + "\nProgram Interrupted By User\n" + Style.RESET_ALL),'\r',
+            print("{0:60}".format(Fore.RED + "\nProgram Interrupted By User\n" + Style.RESET_ALL), end='\r')
             sys.stdout.flush()
-            print ("\nPress Enter to Exit...")
-            raw_input()  
+            print("\nPress Enter to Exit...")
+            input()
             sys.exit(1)
-        if resp=="\xEE":
-            print "{0:60}".format(Fore.RED + "\nChip Unresponsive! Check Connections!\n" + Style.RESET_ALL),'\r',
+        if resp == b'\xEE':
+            print("{0:60}".format(Fore.RED + "\nChip Unresponsive! Check Connections!\n" + Style.RESET_ALL), end='\r')
             sys.stdout.flush()
-            print ("\nPress Enter to Exit...")
-            raw_input()  
+            print("\nPress Enter to Exit...")
+            input()
             sys.exit(1)
-        if resp=="\x00":
-            print "{0:60}".format(Fore.YELLOW + "Glitching..." + Style.RESET_ALL),'\r',
+        if resp == b'\x00':
+            print("{0:60}".format(Fore.YELLOW + "Glitching..." + Style.RESET_ALL), end='\r')
             sys.stdout.flush()
-        if resp =="\x91":
+        if resp == b'\x91':
             print("{0:60}".format(Fore.GREEN + "Chip Responded 0x91 (OCD Connect CMD)" + Style.RESET_ALL))
             while 1:
                 resp = ser.read(1)
-                if resp =="\x94":
+                if resp == b'\x94':
                     print(Fore.GREEN + "Chip Responded 0x94 (OCD EXEC CMD)\n" + Style.RESET_ALL)
-                    print "Saving Dump As {}\n".format(s)
+                    print("Saving Dump As {}\n".format(s))
                     f = open(s, 'wb')
                     f.close()
-                    size = BLOCKSIZE 
+                    size = BLOCKSIZE
                     counter = 0
                     ser.read(1)
                     break
@@ -127,104 +126,99 @@ def dump(s):
             counter += size
             size = BLOCKSIZE
             f = open(s, 'ab')
-            data = str(data)
             f.write(data)
             f.close()
-            print "Dumping: {}/512KB".format(os.stat(s).st_size/1023),'\r',
+            print("Dumping: {}/512KB".format(os.stat(s).st_size // 1024), end='\r')
             sys.stdout.flush()
-            if counter>=(BLOCKSIZE*512):
+            if counter >= (BLOCKSIZE * 512):
                 print(Fore.GREEN + "\n\nDone!" + Style.RESET_ALL)
-                print ("\nElapsed Time: {:0.4f} Seconds\n".format(time.time() - start_time))
+                print("\nElapsed Time: {:0.4f} Seconds\n".format(time.time() - start_time))
                 ser.close()
                 time.sleep(1)
                 break
         except serial.serialutil.SerialException as e:
-            print "{0:60}".format(Fore.RED + "\n\nSerial Exception: Access Denied (USB Unplugged?)\n" + Style.RESET_ALL),'\r',
+            print("{0:60}".format(Fore.RED + "\n\nSerial Exception: Access Denied (USB Unplugged?)\n" + Style.RESET_ALL), end='\r')
             sys.stdout.flush()
-            print ("\nPress Enter to Exit...")
-            raw_input()  
+            print("\nPress Enter to Exit...")
+            input()
             sys.exit(1)
         except KeyboardInterrupt:
-            print "{0:60}".format(Fore.RED + "\n\nProgram Interrupted By User\n" + Style.RESET_ALL),'\r',
+            print("{0:60}".format(Fore.RED + "\n\nProgram Interrupted By User\n" + Style.RESET_ALL), end='\r')
             sys.stdout.flush()
-            print ("\nPress Enter to Exit...")
-            raw_input()  
+            print("\nPress Enter to Exit...")
+            input()
             sys.exit(1)
 
 def main(port):
-	if  len(port) <= 1:
-		print (Fore.RED + "\nError: Invalid COM Port!" + Style.RESET_ALL)
-		print ("\nPress Enter to Exit...")
-		raw_input()  
-		sys.exit(1)
-		return 0
+    if len(port) <= 1:
+        print(Fore.RED + "\nError: Invalid COM Port!" + Style.RESET_ALL)
+        print("\nPress Enter to Exit...")
+        input()
+        sys.exit(1)
+        return 0
 
-	os.system('cls')
-	print_banner()
-	f = ["syscon1.bin", "syscon2.bin"]
-	for val in f:
-		dump(val)
-	comp = filecmp.cmp(f[0], f[1])
-	print "=======================================================\n"
-	print "Comparing Files...."
-	if comp:
-		print (Fore.GREEN + "\nFiles Match!" + Style.RESET_ALL)
-		print "\n=======================================================\n"
-		print "Validating Syscon Firmware...."
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print_banner()
+    f = ["syscon1.bin", "syscon2.bin"]
+    for val in f:
+        dump(val)
+    comp = filecmp.cmp(f[0], f[1])
+    print("=======================================================\n")
+    print("Comparing Files....")
+    if comp:
+        print(Fore.GREEN + "\nFiles Match!" + Style.RESET_ALL)
+        print("\n=======================================================\n")
+        print("Validating Syscon Firmware....")
 
-		f = open(f[0], 'rb')
-		f.seek(0x0)
-		data = f.read(0x60000)
-		hash = hashlib.md5(data).hexdigest()
-		hash = hash.upper()
-		key_value_pairs = syscon_hashes.items()
-		if hash in [v for k, v in key_value_pairs]:
-			for number, desired_hash in syscon_hashes.items():
-				if hash == desired_hash:
-					print(Fore.GREEN + "\nSyscon Firmware " + number + " Valid!" + Style.RESET_ALL)
-		else:
-			print (Fore.RED + "\nDanger! Syscon Firmware Invalid!" + Style.RESET_ALL)
-			print ("\nPress Enter to Exit...")
-			raw_input()  
-			sys.exit(1)
+        with open(f[0], 'rb') as f:
+            f.seek(0x0)
+            data = f.read(0x60000)
+            hash = hashlib.md5(data).hexdigest()
+            hash = hash.upper()
+            key_value_pairs = syscon_hashes.items()
+            if hash in [v for k, v in key_value_pairs]:
+                for number, desired_hash in syscon_hashes.items():
+                    if hash == desired_hash:
+                        print(Fore.GREEN + "\nSyscon Firmware " + number + " Valid!" + Style.RESET_ALL)
+            else:
+                print(Fore.RED + "\nDanger! Syscon Firmware Invalid!" + Style.RESET_ALL)
+                print("\nPress Enter to Exit...")
+                input()
+                sys.exit(1)
 
-	else:
-		print (Fore.RED + "\nDanger! Files Do NOT Match!" + Style.RESET_ALL)
-		print ("\nPress Enter to Exit...")
-		raw_input()  
-		sys.exit(1)
+    else:
+        print(Fore.RED + "\nDanger! Files Do NOT Match!" + Style.RESET_ALL)
+        print("\nPress Enter to Exit...")
+        input()
+        sys.exit(1)
 
+    print("\nPress Enter to Exit...")
+    input()
+    sys.exit(1)
 
-	print ("\nPress Enter to Exit...")
-	raw_input()  
-	sys.exit(1)
-
-	
 if __name__ == '__main__':
-	print_banner()
+    print_banner()
 
-	ports = list(serial.tools.list_ports.comports())
-	auto_ports = []
-	for port in ports:
-		if "USB-SERIAL CH340" in port[1]:
-			auto_ports.append(port[0])
+    ports = list(serial.tools.list_ports.comports())
+    auto_ports = []
+    for port in ports:
+        if "USB-SERIAL CH340" in port[1]:
+            auto_ports.append(port[0])
 
-	if auto_ports:
-		if len(auto_ports) > 1:
-			print(Fore.YELLOW + "Multiple Syscon Readers (Or CH340's) Found At " + ", ".join(auto_ports) + Style.RESET_ALL)
-			port = raw_input("Enter COM Port (Example COM4): ")
-		else:
-			print(Fore.GREEN + "Syscon Reader Found At " + auto_ports[0] + Style.RESET_ALL)
-			port = auto_ports[0]
-	else:
-		port = raw_input("Enter COM Port (Example COM4): ")
+    if auto_ports:
+        if len(auto_ports) > 1:
+            print(Fore.YELLOW + "Multiple Syscon Readers (Or CH340's) Found At " + ", ".join(auto_ports) + Style.RESET_ALL)
+            port = input("Enter COM Port (Example COM4): ")
+        else:
+            print(Fore.GREEN + "Syscon Reader Found At " + auto_ports[0] + Style.RESET_ALL)
+            port = auto_ports[0]
+    else:
+        port = input("Enter COM Port (Example COM4): ")
 
+    if not port:
+        print(Fore.RED + "\nError: No port specified. Exiting program." + Style.RESET_ALL)
+        print("\nPress Enter to Exit...")
+        input()
+        sys.exit(1)
 
-	if not port: 
-		print(Fore.RED + "\nError: No port specified. Exiting program." + Style.RESET_ALL)
-		print ("\nPress Enter to Exit...")
-		raw_input()  
-		sys.exit(1) 
-
-	main(port)
-    
+    main(port)
